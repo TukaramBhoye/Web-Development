7ca78fa53cd26ec03c96567c57fe58ed78ee9fdc
Tukaram Bhoye <tukarambhoye2019@gmail.com>
2025-01-26 21:13:58 +0530

    assignment question

--- Javascript_9.js
+++ Javascript_9.js
@@ -303,7 +303,8 @@ img.setAttribute('src', './creation_3.');
 
 
 */ 
-// Manipulating style 
+// Manipulating style
+/* 
 let img1 = document.querySelector('img'); 
 console.dir(img1); 
 
@@ -329,7 +330,7 @@ for(let i=0; i<links.length; i++){
 }
 
 let box = document.querySelector('.box'); 
-
+*/ 
 
 // Practice Qs. 
 // Add the following elements to the container using only Javascript and the DOM methods. 
@@ -341,12 +342,25 @@ document.querySelector("body").append(para1);
 para1.classList.add("red"); 
 
 // ii: an <h3> with blue text that says "i'm a blue h3!"
-    let h3 = document.createElelement("h3"); 
-    h3.innerText = "I'm a blue h3"; 
-    document.querySelector("body"). append(h3); 
+let h3 = document.createElement("h3"); 
+h3.innerText = "I'm a blue h3!"; 
+document.querySelector("body").append(h3); 
 
-    h3.classList.add("red"); 
+h3.classList.add("blue"); 
+
 // iii: a <div> with a black border and pink background color with the follwoing elements inside of it: 
+let div = document.createElement("div"); 
+let h1 = document.createElement("h1"); 
+let para2 = document.createElement("p"); 
+
+h1.innerText = "Hi I'm div"; 
+para2.innerText = "ME TOO!"; 
+
+div.append(h1); 
+div.append(para2); 
+div.classList.add("box"); 
+document.querySelector("body").append(div);  
+
 
 
 
